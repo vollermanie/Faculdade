d0858c99dcacafd6e539cc7233b6c3d43b9580a2
diegopnf <36850563+diegopnf@users.noreply.github.com>
2020-04-22 21:16:56 -0300

    Atividade 2

---
+++ Praticas de Programacao Integrada 2020/Atividade_2_Praticas_Programacao/src/dao/PaisDAO.java
@@ -0,0 +1,163 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Pais;
+
+public class PaisDAO {
+	
+	public int criar(Pais pais) {
+		String sqlInsert = "INSERT INTO pais(Nome, Populacao, Area) VALUES (?, ?, ?)";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setString(1, pais.getNome());
+			stm.setLong(2, pais.getPopulacao());
+			stm.setDouble(3, pais.getArea());
+			stm.execute();
+			String sqlQuery = "SELECT LAST_INSERT_ID()";
+			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
+					ResultSet rs = stm2.executeQuery();) {
+				if (rs.next()) {
+					pais.setId(rs.getInt(1));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pais.getId();
+	}
+
+	public void atualizar(Pais pais) {
+		String sqlUpdate = "UPDATE Pais SET Nome=?, Populacao=?, Area=? WHERE id=?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
+			stm.setString(1, pais.getNome());
+			stm.setLong(2, pais.getPopulacao());
+			stm.setDouble(3, pais.getArea());
+			stm.setInt(4, pais.getId());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void excluir(int id) {
+		String sqlDelete = "DELETE FROM pais WHERE id = ?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+			stm.setInt(1, id);
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Pais carregar(int id) {
+		Pais pais = new Pais();
+		pais.setId(id);
+		String sqlSelect = "SELECT Nome, Populacao, Area FROM pais WHERE pais.id = ?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, pais.getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					pais.setNome(rs.getString("Nome"));
+					pais.setPopulacao(rs.getLong("Populacao"));
+					pais.setArea(rs.getDouble("Area"));
+				} else {
+					pais.setId(-1);
+					pais.setNome(null);
+					pais.setPopulacao(0);
+					pais.setArea(0);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return pais;
+	}
+	
+	
+	public Pais carregarMax(int id) {
+		Pais pais = new Pais();
+		String sqlSelect = "select Nome,Populacao,Area from pais where Populacao = (select max(populacao) from pais);";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					pais.setNome(rs.getString("Nome"));
+					pais.setPopulacao(rs.getLong("Populacao"));
+					pais.setArea(rs.getDouble("Area"));
+				} else {
+					pais.setId(-1);
+					pais.setNome(null);
+					pais.setPopulacao(0);
+					pais.setArea(0);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return pais;
+	}
+	
+	public Pais carregarMin(int id) {
+		Pais pais = new Pais();
+		String sqlSelect = "select Nome,Populacao,Area from pais where Area = (select min(area) from pais);";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					pais.setNome(rs.getString("Nome"));
+					pais.setPopulacao(rs.getLong("Populacao"));
+					pais.setArea(rs.getDouble("Area"));
+				} else {
+					pais.setId(-1);
+					pais.setNome(null);
+					pais.setPopulacao(0);
+					pais.setArea(0);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		} 
+		return pais;
+	}
+	
+	public Pais vetor() {
+		Pais pais =new Pais();
+		int count = 0;
+		String sqlSelect = "SELECT nome, populacao, area FROM pais limit 3";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			try (ResultSet rs = stm.executeQuery();) {
+				while (rs.next()) {
+					String nome = rs.getString("Nome");
+					long populacao = rs.getLong("Populacao");
+				    double area = rs.getDouble("Area");
+					pais = new Pais(nome,populacao,area);
+					pais.vetor[count++] = pais;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return pais;
+	}
+	
+	
+
+}
---
+++ Praticas de Programacao Integrada 2020/Atividade_2_Praticas_Programacao/src/service/PaisService.java
@@ -0,0 +1,39 @@
+package service;
+
+import model.Pais;
+import dao.PaisDAO;
+
+
+public class PaisService {
+	PaisDAO dao = new PaisDAO();
+	
+	public int criar(Pais pais) {
+		return dao.criar(pais);
+	}
+	
+	public void atualizar(Pais pais){
+		dao.atualizar(pais);
+	}
+	
+	public void excluir(int id){
+		dao.excluir(id);
+	}
+	
+	public Pais carregar(int id){
+		return dao.carregar(id);
+	}
+	
+	public Pais carregarMax(int id) {
+		return dao.carregarMax(id);
+	}
+	
+	public Pais carregarMin(int id) {
+		return dao.carregarMin(id);
+	}
+	
+	public Pais vetor() {
+		return dao.vetor();
+		
+	}
+
+}
